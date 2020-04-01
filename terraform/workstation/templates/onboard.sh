130960b74a7d6460e07b595ccf63eb5c96387cf0
vinnie <vinnie.mazza@gmail.com>
2020-04-01 14:14:18 -0400

    add gcloud cli

--- terraform/workstation/templates/onboard.sh
+++ terraform/workstation/templates/onboard.sh
@@ -15,9 +15,11 @@ repositories="${repositories}"
 user="${user}"
 set -ex \
 && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - \
+&& curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add - \
+&& echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list \
 && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
 && sudo apt-get update -y \
-&& sudo apt-get install -y apt-transport-https wget unzip jq git software-properties-common python3-pip ca-certificates gnupg-agent docker-ce docker-ce-cli containerd.io \
+&& sudo apt-get install -y apt-transport-https wget unzip jq git software-properties-common python3-pip ca-certificates gnupg-agent docker-ce docker-ce-cli containerd.io google-cloud-sdk \
 && echo "docker" \
 && sudo usermod -aG docker $user \
 && sudo chown -R $user: /var/run/docker.sock \
@@ -47,6 +49,7 @@ echo "inspec:"
 inspec version
 terragrunt -version
 f5 --version
+gcloud version
 echo "=====Installed Versions====="
 EOF
 echo "clone repositories"
