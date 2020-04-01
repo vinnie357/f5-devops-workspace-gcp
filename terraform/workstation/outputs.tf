b5772ce3cd5c3f3aa9920d1d9ea87556bf8ff33a
vinnie <vinnie.mazza@gmail.com>
2020-04-01 13:42:15 -0400

    reformat output

--- terraform/workstation/outputs.tf
+++ terraform/workstation/outputs.tf
@@ -3,6 +3,6 @@ output "device_mgmt_ips" {
   #  {"i-1234" = "192.168.1.2", "i-5678" = "192.168.1.5"}
   value = {
     for instance in google_compute_instance.vm_instance:
-    instance.name => "https://${instance.network_interface.0.access_config.0.nat_ip}"
+    instance.name => "${var.adminAccountName}@${instance.network_interface.0.access_config.0.nat_ip}"
   }
 }
